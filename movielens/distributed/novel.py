e1bec2e7ea394da5bd0a286ee83cdde8688b413f
Serban Stan <serbanandrei.stan@gmail.com>
2017-01-18 00:14:23 -0500

    added ratings to movielens

--- movielens/distributed/novel.py
+++ movielens/distributed/novel.py
@@ -11,7 +11,7 @@
 
 import numpy as np
 
-def wrapper(n, m, l, k, sim, movies):
+def wrapper(n, m, l, k, allSim, allMovies):
 
 	# return the set of l elements that maximizes the greedy approach
 	def novel(partition):
@@ -19,15 +19,24 @@ def wrapper(n, m, l, k, sim, movies):
 		# retrieve the elements from this partition - speed improvement
 		local = [i for i in partition]
 
+		# update sim and movies to cater for this partition only
+		sim = dict()
+		movies = dict()
+
+		for i in local:
+			sim[i] = allSim[i]
+		for category in allMovies:
+			movies[category] = list(set(allMovies[category]).intersection(set(local)))
+
 		# work forthe first k entries
-		S, picked = preK(local)
+		S, picked = preK(local, sim, movies)
 
 		# the best S sets for each category
 		bestS = [S for i in range(m)]
 
 		oldCost = 0
 		for i in range(m):
-			oldCost = oldCost + computeCost(i, bestS[i])
+			oldCost = oldCost + computeCost(i, bestS[i], sim, movies)
 
 		for times in range(l - k):
 			bestCost = -1
@@ -43,7 +52,7 @@ def wrapper(n, m, l, k, sim, movies):
 					# now, for each category, swap out the element that gives the least marginal gain
 					for i in range(m):
 
-						catOld = computeCost(i, bestS[i])
+						catOld = computeCost(i, bestS[i], sim, movies)
 						catNew = catOld
 
 						# iterate through all k positions and rememeber the best swap
@@ -52,7 +61,7 @@ def wrapper(n, m, l, k, sim, movies):
 
 							bestS[i][j] = local[movInd]
 
-							val = computeCost(i, bestS[i])
+							val = computeCost(i, bestS[i], sim, movies)
 							if val > catNew:
 								catNew = val
 								swp[i] = j
@@ -77,15 +86,15 @@ def wrapper(n, m, l, k, sim, movies):
 				oldCost = bestCost
 
 
-		totalCost = 0
-		for i in range(m):
-			totalCost = totalCost + computeCost(i, bestS[i])
-		print 'Our solution gives totalCost = ', totalCost
+		# totalCost = 0
+		# for i in range(m):
+		# 	totalCost = totalCost + computeRealCost(i, bestS[i])
+		# print 'Our solution gives totalCost = ', totalCost
 
 		return S
 
 	# before hitting k elements, each element will just have to maximize the marginal gain
-	def preK(local):
+	def preK(local, sim, movies):
 		S = []
 
 		picked = [False for i in range(len(local))]
@@ -102,7 +111,7 @@ def wrapper(n, m, l, k, sim, movies):
 
 					curCost = 0
 					for i in range(m):
-						curCost = curCost + computeCost(i, S)
+						curCost = curCost + computeCost(i, S, sim, movies)
 
 					if curCost > bestCost:
 						bestCost = curCost
@@ -124,7 +133,7 @@ def wrapper(n, m, l, k, sim, movies):
 
 	# write a function that computes the value of f for each category
 	# this is FacilityLocaltion from - Learning mixtures of submodular functions for image collection summarization.
-	def computeCost(catIndex, S):
+	def computeCost(catIndex, S, sim, movies):
 		tot = 0
 
 		for mov in movies[catIndex]:
@@ -132,6 +141,21 @@ def wrapper(n, m, l, k, sim, movies):
 
 			for s in S:
 				mostSim = max(mostSim, np.dot(sim[mov], sim[s]))
+
+			tot = tot + mostSim
+
+		return tot
+
+	# the computeCost function only computes the cost w.r.t. the set it's working with. this function computes the cost w.r.t.
+	# all the elements
+	def computeRealCost(catIndex, S):
+		tot = 0
+
+		for mov in allMovies[catIndex]:
+			mostSim = 0
+
+			for s in S:
+				mostSim = max(mostSim, np.dot(allSim[mov], allSim[s]))
 
 			tot = tot + mostSim
 
